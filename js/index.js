d63efd5a986ef6948abda8bba72f82d306245c23
simon-jo8 <112388673+simon-jo8@users.noreply.github.com>
2024-01-31 09:48:09 +0100

    Snow

--- js/index.js
+++ js/index.js
@@ -6,3 +6,58 @@ document.querySelectorAll('.button').forEach(button => {
         }, 1000); // 1000 milliseconds = 1 second
     });
 });
+
+document.querySelector('.button1').addEventListener('click', () => {
+    let snow = document.querySelector('.snow');
+    snow.classList.toggle('active');
+    setTimeout(() => {
+        snow.classList.toggle('active');
+    }, 300); // 1000 milliseconds = 1 second
+});
+
+
+
+var canvas = document.getElementById('canvas'),
+    ctx = canvas.getContext('2d');
+function resize() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+resize();
+window.onresize = resize;
+
+function generate_noise(ctx, chunkSize = 5) {
+    var w = ctx.canvas.width,
+        h = ctx.canvas.height,
+        idata = ctx.createImageData(w, h),
+        buffer32 = new Uint32Array(idata.data.buffer),
+        w_chunks = Math.ceil(w / chunkSize),
+        h_chunks = Math.ceil(h / chunkSize);
+
+    for (var y = 0; y < h_chunks; y++) {
+        for (var x = 0; x < w_chunks; x++) {
+            var value = ((255 * Math.random()) | 0) << 24;
+            for (var dy = 0; dy < chunkSize; dy++) {
+                for (var dx = 0; dx < chunkSize; dx++) {
+                    var idx = (y * chunkSize + dy) * w + (x * chunkSize + dx);
+                    if (idx < buffer32.length) {
+                        buffer32[idx] = value;
+                    }
+                }
+            }
+        }
+    }
+
+    ctx.putImageData(idata, 0, 0);
+}
+
+var toggle = true;
+(function loop() {
+    toggle = !toggle;
+    if (toggle) {
+        requestAnimationFrame(loop);
+        return;
+    }
+    generate_noise(ctx);
+    requestAnimationFrame(loop);
+})();
